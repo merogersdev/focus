968be9de5c5648158ecb22a49dd0b5ff6702296c
Michelle Rogers <michelle@merogers.dev>
2023-10-27 15:40:54 -0600

    chore: add redux boilerplate

---
+++ app/redux/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import timeReducer from "./time.slice";
+
+export const store = configureStore({
+  reducer: {
+    time: timeReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
